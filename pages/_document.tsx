6487d926313afe5c38ac5da2aa9da2691594e3dc
johnny2smooth <johnbp13@gmail.com>
2023-02-03 10:24:45 -0800

    changed OG to reflect my changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,29 +6,13 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Generate your next Twitter bio in seconds."
-          />
-          <meta property="og:site_name" content="twitterbio.com" />
-          <meta
-            property="og:description"
-            content="Generate your next Twitter bio in seconds."
-          />
-          <meta property="og:title" content="Twitter Bio Generator" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Twitter Bio Generator" />
-          <meta
-            name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
-          />
+          <meta name="description" content="Book Recommendations" />
+          <meta property="og:site_name" content="book-rec.vercel.app" />
+          <meta property="og:description" content="Book Recommendations" />
+          <meta property="og:title" content="AI generated Book Rec" />
           <meta
             property="og:image"
-            content="https://twitterbio.com/og-image.png"
-          />
-          <meta
-            name="twitter:image"
-            content="https://twitterbio.com/og-image.png"
+            content="https://book-rec.vercel.app/berserk.png"
           />
         </Head>
         <body>
